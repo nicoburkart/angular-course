d0f26d3f8b3a4bb30a930b24b18121c53e16899e
Nico Burkart <nb094@hdm-stuttgart.de>
2020-03-10 17:36:18 +0100

    Auto-generated commit

--- out-tsc/src/app/make-nouns-big.pipe.spec.js
+++
@@ -1,8 +0,0 @@
-import { MakeNounsBigPipe } from './make-nouns-big.pipe';
-describe('MakeNounsBigPipe', () => {
-    it('create an instance', () => {
-        const pipe = new MakeNounsBigPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
-//# sourceMappingURL=make-nouns-big.pipe.spec.js.map
